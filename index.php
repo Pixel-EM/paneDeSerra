e63dfaff5f0cc81dafe4d964ab3f9cf19fac40d8
Gabriel <gabrieldoissteam@hotmail.com>
2021-08-02 20:24:43 -0300

    index

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="style.css">
+        <title>Pane di Serra</title>
+    </head>
+    <body>
+        <div class="wrapper">
+            <header>
+                <div id="titlediv">
+                    <h1>Olá, Bem Vindo(a) ao Pane di Serra</h1>
+                </div>
+            </header>
+            <div id="content">
+                <div class="content_button" id="option1">
+                    <a href="pagpaes.php">
+                        <p>Pães</p>
+                    </a>
+                </div>
+                <div class="content_button" id="option2">
+                    <a href="#">
+                        <p>Casa</p>
+                    </a>
+                </div>
+            </div>
+            
+            <script type="module" src="./scripts/receitas.js"></script>
+        </div>
+    </body>
+</html>
---
+++ pagpaes.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="style.css">
+        <title>Pane di Serra</title>
+    </head>
+    <body>
+        <div class="wrapper">
+            <header>
+                <div id="titlediv">
+                    
+                    <img src="" alt="Icone">
+                    <h1>Pane di Serra</h1>
+                </div>
+            </header>
+            <div id="ppcontent">
+                <div id="topicons">
+                    <button type="button" class="btn btn-success">Cadastrar Pão</button>
+                    <button type="button" class="btn btn-info">Editar Pão</button>
+                </div>
+                <div id="vendas">
+                    <div id="totalvendas">
+                        <label for="">Total de Vendas no Mês: <b>mes</b></label>
+                        <b><p>20</p></b>
+                    </div>
+                    <div id="maisvendido">
+                        <label for="">Pão mais vendido:</label>
+                        <b><p>Australiano</p></b>
+                    </div>
+                </div>
+                <div id="conversao">
+                    <div id="selecionarpao">
+                        <select name="" id="selectpao">
+                            <option value="">Pão</option>
+                        </select>
+                    </div>
+                    <div id="valoresconversao">
+                        <div class="vcdiv">
+                            <label for="">Valor Total de Custo:</label>
+                            <b><p>Australiano</p></b>
+                        </div>
+                        <div class="vcdiv">
+                            <label for="">Valor Total de Venda:</label>
+                            <b><p>Australiano</p></b>
+                        </div>
+                        <div class="vcdiv">
+                            <label for="">Valor de Ganho:</label>
+                            <b><p>Australiano</p></b>
+                        </div>
+                    </div>
+                </div>
+                <div id="receita">
+
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
